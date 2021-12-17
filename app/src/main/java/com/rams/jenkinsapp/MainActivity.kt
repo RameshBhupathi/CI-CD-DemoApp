85b348cc05c850196a136bb54b706a26b82af27e
bramesh <bramesh@ivycomptech.com>
2021-12-17 10:36:21 +0530

    Added splash.

--- app/src/main/java/com/rams/jenkinsapp/MainActivity.kt
+++ app/src/main/java/com/rams/jenkinsapp/MainActivity.kt
@@ -3,10 +3,16 @@ package com.rams.jenkinsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.rams.jenkinsapp.ui.theme.JenkinsAppTheme
 
@@ -16,8 +22,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             JenkinsAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.onPrimary) {
+                    Greeting()
                 }
             }
         }
@@ -25,19 +31,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Image(painterResource(id = R.drawable.banner_image_new),
+        contentDescription = "haaaa",
+        modifier = Modifier.fillMaxWidth().fillMaxHeight()
+    )
+    GreetingText()
 }
 
 @Composable
-fun DummyGreeting(name: String) {
-    Text(text = "Dummy Hello $name!")
+fun GreetingText() {
+    Text(text = "WellCome to CI-CD")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JenkinsAppTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
---
+++ app/src/main/java/com/rams/jenkinsapp/SplashActivity.kt
@@ -0,0 +1,47 @@
+package com.rams.jenkinsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowInsets
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import com.rams.jenkinsapp.databinding.ActivitySplashBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        lifecycleScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main){
+                launchHome()
+            }
+        }
+    }
+
+    private fun launchHome() {
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
+    }
+
+}
